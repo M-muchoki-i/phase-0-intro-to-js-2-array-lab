1f0977784ddfdacd6e1e6f93792547251e960e9a
michelle <michellemuchoki8@gmail.com>
2025-04-01 19:46:05 +0300

    array done

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+console.log(cats);
+// console.log(cats);
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+destructivelyAppendCat["Ralph"];
+console.log(cats);
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+destructivelyPrependCat["Bob"];
+console.log(cats);
+
+function destructivelyRemoveLastCat() {
+  cats.pop(cats);
+}
+console.log(cats);
+
+function destructivelyRemoveFirstCat() {
+  cats.shift(cats);
+}
+console.log(cats);
+
+function appendCat(name) {
+  return [...cats, name];
+}
+let newCats = appendCat("Broom");
+console.log(cats);
+
+ const originalCats = [...cats];
+
+function prependCat(name) {
+  return [name,...cats];
+}
+ const newestCats = prependCat("Arnold");
+console.log(newestCats);
+
+ const removedLastCat = removeLastCat();
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+const removedFirstCat = removeFirstCat();
+ 
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+
+console.log("Original Cats:", cats); 
+console.log("Removed Last Cat:", removedLastCat);
+
+
+
+console.log("Original Cats:", cats);
+console.log("Removed First Cat:", removedFirstCat);
